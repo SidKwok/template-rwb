8eacebb2a011a924b9d5f44b3c62a9d32a71cfc7
SidKwok <oceankwok@hotmail.com>
2017-03-15 11:02:06 +0800

    switch `babel-preset-es2015` to `babel-preset-env`

--- template/src/components/Hello.jsx
+++ template/src/components/Hello.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
 import './Hello.css'
 
-export default class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      msg: 'Hello World!'
-    }
+export default class Hello extends Component {
+  state = {
+    msg: 'Hello World!'
   }
   render () {
     return (
