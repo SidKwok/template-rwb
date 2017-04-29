31efba743b67046fcb4fbb1828fa5af3f4dd6011
SidKwok <oceankwok@hotmail.com>
2017-04-29 12:41:40 +0800

    :rotating_light:

--- template/src/routes/index.js
+++ template/src/routes/index.js
@@ -27,7 +27,7 @@ export default [
         component: Loadable({
           loader: () => import(`views/AsyncView`),
           // if you have your own loading component,
-          // you should consider add it here  
+          // you should consider add it here
           LoadingComponent: () => null
         })
         {{/if_eq}}
