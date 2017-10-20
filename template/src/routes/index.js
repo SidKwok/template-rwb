71fa956d361f3242fc46e3ebda968ae70f8dfd47
SidKwok <oceankwok@hotmail.com>
2017-10-20 14:42:57 +0800

    fix typo

--- template/src/routes/index.js
+++ template/src/routes/index.js
@@ -3,7 +3,7 @@ import MainView from 'views/MainView'
 import SyncView from 'views/SyncView'
 
 const AsyncView = Loadable({
-  loader: () => import(`views/AsyncView`),
+  loader: () => import('views/AsyncView'),
   // if you have your own loading component,
   // you should consider add it here
   loading: () => null
